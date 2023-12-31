50556e1a02caa99a9e52e4310aedf7b6a4d9ea3b
minsangOh <flos9537@gmail.com>
2023-12-31 15:09:40 +0900

    Revert "에러발생"
    This reverts commit b6d1a3b9fce6c6c132cabea56ae4f5161cad0558.

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,54 +1,9 @@
-import { getAllPostIds, getPostData, getSortedPsotsData } from "@/lib/posts";
-import { GetStaticPaths, GetStaticProps } from "next";
-import Head from "next/head";
-import React from "react";
+import React from 'react'
 
-const Post = ({
-  postData,
-}: {
-  postData: {
-    title: string;
-    date: string;
-    contentHtml: string;
-  };
-}) => {
+const Post = () => {
   return (
-    <div>
-      <Head>
-        <title> {postData.title} </title>
-      </Head>
-      <article>
-        <h1> {postData.title} </h1>
-        <div>
-          {postData.date}
-        </div>
-        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}>
-
-        </div>
-      </article>
-
-    </div>
+    <div>[id]</div>
   )
-};
+}
 
-export default Post;
-
-export const getstaticPath: GetStaticPaths = async () => {
-  const paths = getAllPostIds();
-  // [{params: { id: 'pre-rendering'}. {param...}}] 이런식으로 들어있음
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postFata = await getPostData(params.id as string);
-
-  return {
-    props: {
-      postData,
-    },
-  };
-};
+export default Post
