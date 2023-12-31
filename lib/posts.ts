b6d1a3b9fce6c6c132cabea56ae4f5161cad0558
minsangOh <flos9537@gmail.com>
2023-12-31 15:09:32 +0900

    에러발생

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+
 const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPsotsData() {
@@ -17,9 +20,9 @@ export function getSortedPsotsData() {
 
     return {
       id,
-      ...matterResult.data as { date: string; title: string },
+      ...(matterResult.data as { date: string; title: string }),
     };
-  }); 
+  });
 
   // Sorting
   return allpostsData.sort((a, b) => {
@@ -29,4 +32,34 @@ export function getSortedPsotsData() {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  const matterResult = matter(fileContents);
+
+  // remark 사용하려면 npm install remark remark-html 해줘야함.
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as {date: string, title:string})
+  }
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { getAllPostIds, getPostData, getSortedPsotsData } from "@/lib/posts";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
 
-const Post = () => {
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
   return (
-    <div>[id]</div>
-  )
-}
+    <div>
+      <Head>
+        <title> {postData.title} </title>
+      </Head>
+      <article>
+        <h1> {postData.title} </h1>
+        <div>
+          {postData.date}
+        </div>
+        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}>
 
-export default Post
+        </div>
+      </article>
+
+    </div>
+  )
+};
+
+export default Post;
+
+export const getstaticPath: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: { id: 'pre-rendering'}. {param...}}] 이런식으로 들어있음
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postFata = await getPostData(params.id as string);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
